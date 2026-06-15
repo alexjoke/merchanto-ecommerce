eb2af32fcf6f200c8d7cd0e048d2fc5999d0038a
Alex Russu <alexruss2777@gmail.com>
2026-06-15 17:57:51 +0300

    Fix Pest setup

--- tests/Pest.php
+++ tests/Pest.php
@@ -9,10 +9,7 @@ use Tests\TestCase;
 
 pest()->extend(TestCase::class)
     ->use(RefreshDatabase::class)
-    ->in('Feature', '../Modules/*/tests/Feature');
-
-pest()->extend(TestCase::class)
-    ->in('Unit', '../Modules/*/tests/Unit');
+    ->in('../Modules/*/tests/Feature');
 
 function actingAsAdmin(): User
 {
